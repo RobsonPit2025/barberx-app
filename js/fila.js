5b3e515dec120a060d8774c23b6409d6180586e9
RobsonPit2025 <robson302025@gmail.com>
2025-07-24 15:38:21 -0300

    correção de bugs

--- js/fila.js
+++
@@ -1,9 +0,0 @@
-// js/firebase-config.js
-export const firebaseConfig = {
-  apiKey: "AIzaSyCnsA89psIo30sQdBM9wFFzydnfOLcOKIc",
-  authDomain: "barbex-app.firebaseapp.com",
-  projectId: "barbex-app",
-  storageBucket: "barbex-app.appspot.com",
-  messagingSenderId: "91864465722",
-  appId: "1:91864465722:web:7a3365583f3ca63e19d003"
-};
--- js/index.js
+++ js/index.js
@@ -1,19 +1,29 @@
-
 function mostrarSecao(id) {
-  document.querySelectorAll('.secao').forEach(sec => sec.classList.add('hidden'));
+  document.querySelectorAll('section').forEach(sec => sec.classList.add('hidden'));
   document.getElementById(id).classList.remove('hidden');
+}
+
+function cadastrarUsuario() {
+  const nome = document.getElementById('cadastroNome').value;
+  const email = document.getElementById('cadastroEmail').value;
+  const celular = document.getElementById('cadastroCelular').value;
+  const senha = document.getElementById('cadastroSenha').value;
+
+  if (!nome || !email || !celular || !senha) {
+    alert("Preencha todos os campos");
+    return;
+  }
+
+  alert("Usuário cadastrado com sucesso! (Simulação)");
 }
 
 function fazerLogin() {
   const email = document.getElementById('loginEmail').value;
   const senha = document.getElementById('loginSenha').value;
-  alert(`Login com ${email} (senha escondida)`);
-}
 
-function fazerCadastro() {
-  const nome = document.getElementById('cadastroNome').value;
-  const email = document.getElementById('cadastroEmail').value;
-  const celular = document.getElementById('cadastroCelular').value;
-  const senha = document.getElementById('cadastroSenha').value;
-  alert(`Cadastro de ${nome} realizado!`);
+  if (email === 'admin@barberx.com' && senha === '123456') {
+    alert("Login como admin. (Em breve redireciona)");
+  } else {
+    alert("Login como cliente. (Em breve redireciona)");
+  }
 }
